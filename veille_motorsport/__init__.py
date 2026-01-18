898f3f551714ad119630f0d99a739c319a24079e
NicolasGut <mail@nicolasgut.com>
2026-01-18 19:16:08 +0100

    import score_article_V2 fix

--- veille_motorsport/__init__.py
+++ veille_motorsport/__init__.py
@@ -1,5 +1,5 @@
 """
-Veille Motorsport - Système de veille automatisée
+Veille Motorsport - SystÃ¨me de veille automatisÃ©e
 Pour F1, F2, WEC et sport automobile
 
 Package principal contenant tous les modules de veille.
@@ -11,7 +11,7 @@ __author__ = "Votre Nom"
 # Imports principaux pour faciliter l'utilisation
 from .rss_aggregator import fetch_rss_feeds, filter_recent_articles, save_to_database
 from .article_extractor import extract_full_article, extract_batch_articles
-from .article_scorer import score_article_relevance, rank_articles
+from .article_scorer import score_article_v2, rank_articles, get_top_articles
 from .ai_summarizer import summarize_article_claude, summarize_batch
 from .web_generator import generate_weekly_digest_html, save_weekly_digest
 from .web_scraper import scrape_wec_news, scrape_f1technical_news, scrape_all_sources
@@ -22,8 +22,9 @@ __all__ = [
     'save_to_database',
     'extract_full_article',
     'extract_batch_articles',
-    'score_article_relevance',
+    'score_article_v2',
     'rank_articles',
+    'get_top_articles',
     'summarize_article_claude',
     'summarize_batch',
     'generate_weekly_digest_html',
